c298a4d3882aab67af7df68f7295541f2782482b
Joe Elliott <jelliott@qsrautomations.com>
2019-06-29 22:31:33 -0400

    Added checking loop

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/joe-elliott/cert-exporter/src/certs"
 	"github.com/joe-elliott/cert-exporter/src/globargs"
 
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -16,15 +18,16 @@ var (
 	excludeGlobs            globargs.Args
 	prometheusListenAddress string
 	prometheusPath          string
+	pollingPeriod           time.Duration
 )
 
 func init() {
 
 	flag.Var(&includeGlobs, "include-glob", "File globs to include when looking for certs.")
 	flag.Var(&excludeGlobs, "exclude-glob", "File globs to exclude when looking for certs.")
-
 	flag.StringVar(&prometheusPath, "prometheus-path", "/metrics", "The path to publish Prometheus metrics to.")
 	flag.StringVar(&prometheusListenAddress, "prometheus-listen-address", ":8080", "The address to listen on for Prometheus scrapes.")
+	flag.DurationVar(&pollingPeriod, "polling-period", time.Hour, "Periodic interval in which to check certs.")
 
 	klog.InitFlags(nil)
 }
@@ -33,6 +36,9 @@ func main() {
 	klog.Info("Application Starting")
 
 	flag.Parse()
+
+	c := certs.NewCertChecker(pollingPeriod, includeGlobs, excludeGlobs)
+	go c.StartChecking()
 
 	http.Handle(prometheusPath, promhttp.Handler())
 	log.Fatal(http.ListenAndServe(prometheusListenAddress, nil))
---
+++ src/certs/certs.go
@@ -0,0 +1,74 @@
+package certs
+
+import (
+	"time"
+	"path/filepath"
+
+	"k8s.io/klog"
+)
+
+type PeriodicCertChecker struct {
+	period time.Duration
+	includeGlobs []string
+	excludeGlobs []string
+}
+
+func NewCertChecker(period time.Duration, includeGlobs []string, excludeGlobs []string) *PeriodicCertChecker {
+	return &PeriodicCertChecker{
+		period : period,
+	}
+}
+
+func (p *PeriodicCertChecker) StartChecking() {
+
+	periodChannel := time.Tick(p.period)
+
+	for {
+		<-periodChannel
+
+		for _, match := range p.getMatches() {
+
+			if !p.includeFile(match) {
+				continue
+			}
+
+			klog.Info("Publishing metrics for %v", match)
+		}
+	}
+}
+
+func (p *PeriodicCertChecker) getMatches() []string {
+	ret := make([]string, 1)
+
+	for _, includeGlob := range p.includeGlobs {
+
+		matches, err := filepath.Glob(includeGlob)
+
+		if err != nil {
+			klog.Error("Glob failed on %v: %v", includeGlob, err)
+			continue
+		}
+
+		ret = append(ret, matches...)
+	}
+
+	return ret
+}
+
+func (p *PeriodicCertChecker) includeFile(file string) bool {
+
+	for _, excludeGlob := range p.excludeGlobs {
+		exclude, err := filepath.Match(excludeGlob, file)
+
+		if err != nil {
+			klog.Error("Match failed on %v,%v: %v", excludeGlob, file, err)
+			return false
+		}
+
+		if exclude {
+			return false
+		}
+	}
+
+	return true
+}
