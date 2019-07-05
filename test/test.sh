0b5430ea60b4feb3b05f185b218a48051f3140b3
Joe Elliott <jelliott@qsrautomations.com>
2019-07-04 23:54:34 -0400

    Added some basic tests

--- test/test.sh
+++ test/test.sh
@@ -1,6 +1,32 @@
+set -e
+
+# cleanup certs
 rm -rf certs
 mkdir certs
+rm main
+
+# build
+go build ../main.go
+chmod +x main
 
 ./genCerts.sh certs 100
 ./genKubeConfig.sh certs ./
 
+# run exporter
+./main -include-cert-glob=certs/*.crt  -include-kubeconfig-glob=certs/kubeconfig &
+
+sleep 5
+
+curl --silent http://localhost:8080/metrics | grep 'cert_exporter_error_total 0'
+
+curl --silent http://localhost:8080/metrics | grep 'cert_exporter_cert_expires_in_seconds{filename="certs/client.crt"}'
+curl --silent http://localhost:8080/metrics | grep 'cert_exporter_cert_expires_in_seconds{filename="certs/root.crt"}'
+curl --silent http://localhost:8080/metrics | grep 'cert_exporter_cert_expires_in_seconds{filename="certs/server.crt"}'
+
+curl --silent http://localhost:8080/metrics | grep 'cert_exporter_kubeconfig_expires_in_seconds{filename="certs/kubeconfig",name="cluster1",type="cluster"}'
+curl --silent http://localhost:8080/metrics | grep 'cert_exporter_kubeconfig_expires_in_seconds{filename="certs/kubeconfig",name="cluster2",type="cluster"}'
+curl --silent http://localhost:8080/metrics | grep 'cert_exporter_kubeconfig_expires_in_seconds{filename="certs/kubeconfig",name="user1",type="user"}'
+curl --silent http://localhost:8080/metrics | grep 'cert_exporter_kubeconfig_expires_in_seconds{filename="certs/kubeconfig",name="user2",type="user"}'
+
+# kill exporter
+kill $!
