c8d2912c8f4cb63f2e5f427120e682f4099eac63
Joe Elliott <jelliott@qsrautomations.com>
2019-07-13 20:52:10 -0400

    Added crappy test

--- test/cert-manager/install.sh
+++ test/cert-manager/install.sh
@@ -5,25 +5,3 @@ kubectl label namespace cert-manager certmanager.k8s.io/disable-validation=true
 kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v0.8.1/cert-manager.yaml
 
 kubectl create -f certs.yaml
-
-# labels:
-#   certmanager.k8s.io/certificate-name: selfsigned-cert
-
-# Name:         selfsigned-cert-tls
-# Namespace:    cert-manager-test
-# Labels:       certmanager.k8s.io/certificate-name=selfsigned-cert
-# Annotations:  certmanager.k8s.io/alt-names: example.com
-#               certmanager.k8s.io/common-name: example.com
-#               certmanager.k8s.io/ip-sans: 
-#               certmanager.k8s.io/issuer-kind: Issuer
-#               certmanager.k8s.io/issuer-name: test-selfsigned
-
-# Type:  kubernetes.io/tls
-
-# Data
-# ====
-# ca.crt:   1139 bytes
-# tls.crt:  1139 bytes
-# tls.key:  1675 bytes
-
-# cert data is base64 encoded pem
