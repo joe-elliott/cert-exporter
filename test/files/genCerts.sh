8aa181c1555c28b017d1c5b80f2db386299a4647
Nick Van Cauter <nick.vancauter@outlook.com>
2020-11-09 13:29:13 +0100

    update genCerts.sh and test.sh to test new cert-exporter behaviour

--- test/files/genCerts.sh
+++ test/files/genCerts.sh
@@ -21,15 +21,24 @@ pushd $certFolder
 openssl genrsa -out root.key
 openssl genrsa -out client.key
 openssl genrsa -out server.key
+openssl genrsa -out bundle_root.key
+openssl genrsa -out bundle_server.key
 
-# root cert
+# root cert & bundle root cert
 openssl req -x509 -new -nodes -key root.key -subj "/C=US/ST=KY/O=Org/CN=root" -sha256 -days $days -out root.crt
+openssl req -x509 -new -nodes -key bundle_root.key -subj "/C=BE/ST=EF/O=Org/CN=bundle-root" -sha256 -days $days -out bundle_root.cert
 
 # csrs
 openssl req -new -sha256 -key client.key -subj "/C=US/ST=KY/O=Org/CN=client" -out client.csr
 openssl req -new -sha256 -key server.key -subj "/C=US/ST=KY/O=Org/CN=example.com" -out server.csr
+openssl req -new -sha256 -key bundle_server.key -subj "/C=BE/ST=EF/O=Org/CN=example-bundle.be" -out bundle_server.csr
 
 openssl x509 -req -in client.csr -CA root.crt -CAkey root.key -CAcreateserial -out client.crt -days $days -sha256
 openssl x509 -req -in server.csr -CA root.crt -CAkey root.key -CAcreateserial -out server.crt -days $days -sha256
+openssl x509 -req -in bundle_server.csr -CA bundle_root.cert -CAkey bundle_root.key -CAcreateserial -out bundle_server.cert -days $days -sha256
+
+
+# create bundle
+cat bundle_server.cert bundle_root.cert > bundle.crt
 
 popd
