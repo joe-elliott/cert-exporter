7c5285649543747024476a6d1172a40cac2f51a0
Andrew <35912177+aballman@users.noreply.github.com>
2025-06-10 10:01:30 -0700

    fix: copy namespaces in configmap checker correctly

--- src/checkers/periodicConfigMapChecker.go
+++ src/checkers/periodicConfigMapChecker.go
@@ -87,7 +87,7 @@ func (p *PeriodicConfigMapChecker) StartChecking() {
 				}
 			}
 		} else {
-			copy(namespacesToCheck, p.namespaces)
+			namespacesToCheck = append(namespacesToCheck, p.namespaces...)
 		}
 
 		for _, ns := range namespacesToCheck {
